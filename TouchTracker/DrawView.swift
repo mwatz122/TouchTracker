cf0193e8bd174b53a251c2d95abd679afc1f1946
Matthew A. Watzman <mwatz122@gmail.com>
2018-02-02 12:45:50 -0500

    added DrawView custom view

---
+++ TouchTracker/DrawView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Matthew Watzman on 2/2/18.
+//  Copyright © 2018 Matthew Watzman. All rights reserved.
+//
+
+import UIKit
+
+class DrawView : UIView {
+    var currentLine: Line?
+    var finishedLines = [Line]()
+}
