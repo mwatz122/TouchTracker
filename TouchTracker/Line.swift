4c6d4c3a73f289b362dfaecd8298fa48733cc8e3
Matthew A. Watzman <mwatz122@gmail.com>
2018-02-02 12:36:46 -0500

    added Line struct

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Matthew Watzman on 2/2/18.
+//  Copyright © 2018 Matthew Watzman. All rights reserved.
+//
+
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
